a50a559ef438c2be9434b479611cd33a83c9de90
Abhishek-raj093 <ss0986825@gmail.com>
2025-08-29 15:14:46 +0530

    Completed the FrontEnd

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-// import Navbar from '../components/Navbar'
 import { Route, Routes, useLocation } from 'react-router-dom'
 import Home from './pages/Home'
 import MovieDetails from './pages/MovieDetails'
@@ -10,6 +9,11 @@ import { Toaster } from 'react-hot-toast'
 import Footer from './components/Footer'
 import Navbar from './components/Navbar'
 import Movies from './pages/Movies'
+import Layout from './pages/admin/Layout'
+import Dashboard from './pages/admin/Dashboard'
+import AddShows from './pages/admin/AddShows'
+import ListShows from './pages/admin/ListShows'
+import ListBookings from './pages/admin/ListBookings'
 
 const App = () => {
 
@@ -26,6 +30,12 @@ const App = () => {
         <Route path='/movies/:id/:date' element={<SeatLayout/>} />
         <Route path='/my-bookings' element={<MyBookings/>} />
         <Route path='/favorite' element={<Favorite/>} />
+        <Route path='/admin/*' element={<Layout/>}>
+          <Route index element={<Dashboard/>} />
+          <Route path='add-shows' element={<AddShows/>} />
+          <Route path='list-shows' element={<ListShows/>} />
+          <Route path='list-bookings' element={<ListBookings/>} />
+        </Route>
       </Routes>
       {!isAdminRoute && <Footer /> }
     </>
