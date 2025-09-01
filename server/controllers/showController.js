93f602f78e7f11942852abeb9164bb7ab5e2291e
Abhishek-raj093 <ss0986825@gmail.com>
2025-09-01 22:41:47 +0530

    Created the API to Fetch All Show from the Database

--- server/controllers/showController.js
+++ server/controllers/showController.js
@@ -79,3 +79,18 @@ export const addShow = async (req, res) => {
         res.json({success: false, message: error.message});
     }
 }
+
+// API to get all show from the databse
+export const gatShows = async (req, res)=>{
+    try {
+        const shows = await Show.find({showDateTime: {$gte: new Date()}}).populate('movie').sort({showDateTime: 1});
+
+        // Filter unique shows
+        const uniqueShows = new Set(shows.map(show => show.movie))
+
+        res.json({success: true, shows: Array.from(uniqueShows)})
+    } catch (error) {
+        console.error(error);
+        res.json({ success: false, message: error.message });
+    }
+}
